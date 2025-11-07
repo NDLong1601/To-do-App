e16c02ea2f57050a805f5841875b8e7f2f60e88c
NguyenLong <nguyenlong160104@gmail.com>
2025-11-07 12:46:28 +0700

    update splash & registration

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -14,159 +14,294 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
       ),
-      home: DemoWidget(),
+      home: SplashScense(),
     );
   }
 }
 
-class DemoWidget extends StatelessWidget {
-  const DemoWidget({super.key});
+class SplashScense extends StatelessWidget {
+  const SplashScense({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.back_hand, color: Colors.blue),
-        title: Text(
-          'Demo Widget',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.w700,
-            fontSize: 20,
-            color: Color(0xFF000000).withValues(alpha: 0.74),
-          ),
-        ),
-        backgroundColor: Colors.amberAccent,
-        actions: [Icon(Icons.search), Icon(Icons.search)],
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 20, top: 50, right: 20, bottom: 30),
-            child: Text(
-              'Hello, Flutter!',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Color(0xFF000000).withValues(alpha: 0.87),
+        backgroundColor: Color(0xFFF6F6F6),
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Image(image: AssetImage('assets/images/img_shape.png')),
+            ),
+            SizedBox(height: 59),
+            Image(
+              image: AssetImage('assets/images/img_undraw_mobile.png'),
+              width: 290,
+              height: 270,
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.only(left: 48, right: 46),
+              child: Text(
+                'Gets things done with TODO',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF000000).withValues(alpha: 0.75)),
               ),
             ),
-          ),
-          Text(
-            'Hello, Flutter! adasl hagdslhasdhgashadshasdh asdhasd asdgh sad gh',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w400,
-              fontSize: 16,
-              color: Color(0xFF000000).withValues(alpha: 0.87),
+            SizedBox(height: 36),
+            Padding(
+              padding: EdgeInsets.only(left: 48, right: 46),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF000000).withValues(alpha: 0.74),
+                ),
+              ),
             ),
-          ),
-          // Image.asset('assets/images/img_shape.png'),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DemoWdiget2()),
-              );
-            },
-            child: Image.asset('assets/icons/ic_plus_circle.png'),
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: 'Enter your full name'),
-          ),
-          // SizedBox(
-          //   height: 300,
-          //   child: ListView.separated(
-          //     separatorBuilder: (context, index) => SizedBox(height: 20),
-          //     itemBuilder: (context, index) {
-          //       return Container(
-          //         width: 100,
-          //         height: 100,
-          //         color: index % 2 == 0 ? Colors.red : Colors.green,
-          //       );
-          //     },
-          //     itemCount: 10,
-          //   ),
-          // child: ListView(
-          //   shrinkWrap: true,
-          //   children: [
-          //     Image.asset('assets/images/img_shape.png'),
-          //     Container(width: 100, height: 100, color: Colors.red),
-          //     Container(width: 100, height: 100, color: Colors.green),
-          //     InkWell(
-          //       onTap: () {
-          //         print('Icon tapped!');
-          //       },
-          //       child: Image.asset('assets/icons/ic_plus_circle.png'),
-          //     ),
-          //     Container(width: 100, height: 100, color: Colors.yellow),
-          //     Container(width: 100, height: 100, color: Colors.black),
-          //   ],
-          // ),
-          // ),
-
-          // Stack(
-          //   children: [
-          //     Container(width: 200, height: 200, color: Colors.blue),
-          //     Positioned(
-          //       top: 50,
-          //       left: 50,
-          //       child: Container(width: 100, height: 100, color: Colors.red),
-          //     ),
-          //     Positioned(
-          //       top: 20,
-          //       left: 30,
-          //       child: Container(width: 50, height: 50, color: Colors.green),
-          //     ),
-          //   ],
-          // ),
-
-          // Container(
-          //   decoration: BoxDecoration(
-          //     borderRadius: BorderRadius.circular(16),
-          //     color: Colors.amber,
-          //     border: Border.all(color: Colors.blue, width: 2),
-          //     boxShadow: [
-          //       BoxShadow(
-          //         blurRadius: 15,
-          //         spreadRadius: 0,
-          //         color: Colors.black.withValues(alpha: 0.25),
-          //         offset: Offset(0, 4),
-          //       ),
-          //     ],
-          //   ),
-          //   width: 300,
-          //   height: 200,
-          //   margin: EdgeInsets.only(top: 50),
-          //   padding: EdgeInsets.only(left: 50, top: 20),
-          //   child: Text("This is a container with padding and margin"),
-          // ),
-        ],
-      ),
-    );
+            Container(
+              margin: EdgeInsets.only(top: 92, left: 26, right: 24),
+              width: 325,
+              height: 62,
+              decoration: BoxDecoration(
+                color: Color(0xFF50C2C9),
+                // borderRadius: BorderRadius.circular(10),
+              ),
+              // child: Center(
+              //   child: Text(
+              //     'Get Started',
+              //     style: TextStyle(
+              //       fontFamily: 'Poppins',
+              //       fontSize: 16,
+              //       fontWeight: FontWeight.w600,
+              //       color: Color(0xFFFFFFFF),
+              //     ),
+              //   ),
+              // ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                        builder: (context) => RegistrationScense()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF50C2C9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize: Size(325, 62),
+                ),
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
 
-class DemoWdiget2 extends StatefulWidget {
-  const DemoWdiget2({super.key});
+class RegistrationScense extends StatelessWidget {
+  const RegistrationScense({super.key});
 
-  @override
-  State<DemoWdiget2> createState() => _DemoWdiget2State();
-}
-
-class _DemoWdiget2State extends State<DemoWdiget2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Demo Widget 2')),
-      body: InkWell(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Center(child: Text('This ')),
-      ),
+        backgroundColor: Color(0xFFF6F6F6),
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Image.asset('assets/images/img_shape.png'),
+            ),
+            SizedBox(height: 76),
+            Text(
+              'Welcome Onboard!',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Color(0xFF000000).withValues(alpha: 0.75),
+              ),
+            ),
+            SizedBox(height: 14),
+            Padding(
+              padding: EdgeInsets.only(left: 69, right: 65),
+              child: Text(
+                'Let’s help you meet up your tasks',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF000000).withValues(alpha: 0.74),
+                ),
+              ),
+            ),
+            SizedBox(height: 49),
+            Padding(
+              padding: const EdgeInsets.only(left: 27, right: 23),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your full name',
+                  filled: true,
+                  fillColor: Color(0xFFFFFFFF),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                ),
+              ),
+            ),
+            SizedBox(height: 21),
+            Padding(
+              padding: const EdgeInsets.only(left: 27, right: 23),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  filled: true,
+                  fillColor: Color(0xFFFFFFFF),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                ),
+              ),
+            ),
+            SizedBox(height: 21),
+            Padding(
+              padding: const EdgeInsets.only(left: 27, right: 23),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  filled: true,
+                  fillColor: Color(0xFFFFFFFF),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                ),
+              ),
+            ),
+            SizedBox(height: 21),
+            Padding(
+              padding: const EdgeInsets.only(left: 27, right: 23),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Confirm Password',
+                  filled: true,
+                  fillColor: Color(0xFFFFFFFF),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Container(
+              width: 325,
+              height: 62,
+              decoration: BoxDecoration(
+                color: Color(0xFF50C2C9),
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                        builder: (context) => DashboardScense()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF50C2C9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize: Size(325, 62),
+                ),
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 23),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Already have an account ?',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    )),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute<void>(
+                          builder: (context) => SignInScense()),
+                    );
+                  },
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF50C2C9),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
+
+class SignInScense extends StatelessWidget {
+  const SignInScense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class DashboardScense extends StatelessWidget {
+  const DashboardScense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('data'),
     );
   }
 }
